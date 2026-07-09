e95c2152e80e68e26607f26e35653b0e9ae51267
aegis <1160490625@qq.com>
2026-07-09 12:45:28 +0800

    update

--- install_verl.sh
+++ install_verl.sh
@@ -1,13 +1,17 @@
 #! /bin/bash
 
+# CUDA 12.8
 # clone verl source code
 git clone git@github.com:jxfzzzt/verl-source.git
 cd verl-source
 
 # create conda environment
-
 conda create -n verl python=3.10 -y
 conda activate verl
-pip install -e .
 
+# install the dependencies
+pip install torch==2.8.0 torchvision==0.23.0 torchaudio==2.8.0 --index-url https://download.pytorch.org/whl/cu128
+pip install transformers==4.56.1
+pip install -e .
 pip install -r requirements.txt
+
