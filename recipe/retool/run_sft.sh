251a58c4d15f96eabd29144e1bbb806456437c92
aegis <1160490625@qq.com>
2026-07-13 00:21:13 +0800

    update retool sft bash

--- recipe/retool/run_sft.sh
+++ recipe/retool/run_sft.sh
@@ -22,19 +22,19 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --standalone --nnodes=1 --nproc_per_node=4
      -m verl.trainer.fsdp_sft_trainer \
     data.train_files=$TRAIN_DATA \
     data.val_files=$EVAL_DATA \
-    data.max_length=4096 \
-    data.train_batch_size=8 \
+    data.max_length=16384 \
+    data.train_batch_size=1 \
     data.multiturn.enable=true \
     data.multiturn.messages_key=messages \
     data.multiturn.tools_key=tools \
-    data.micro_batch_size_per_gpu=2 \
+    data.micro_batch_size_per_gpu=1 \
     model.partial_pretrain=$MODEL_PATH \
     model.trust_remote_code=true \
     trainer.default_local_dir=$SAVE_PATH \
     trainer.project_name=multiturn-sft \
     trainer.experiment_name=multiturn-sft-qwen-3-8b-instruct-sp2 \
     trainer.logger=['console','wandb'] \
-    trainer.total_epochs=2 \
+    trainer.total_epochs=1 \
     trainer.default_hdfs_dir=null \
     ulysses_sequence_parallel_size=4 \
     use_remove_padding=true
